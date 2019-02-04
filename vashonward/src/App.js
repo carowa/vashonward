ebc540cc99a24b89a6b1aae1a6118d4f40cb53fc
Katherine Goulding <katiegoulding@gmail.com>
2019-02-03 16:05:48 -0800

    add additional barebones changes

--- vashonward/src/App.js
+++ vashonward/src/App.js
@@ -27,11 +27,11 @@ class App extends Component {
             [offset 2/12 ] [ img 3/12 screen ] [text 4/12]
         
         */}
-            <div className="center-xs col-xs-12 col-sm-4 col-md-offset-2 col-md-2">
+            <div className="center-xs col-xs-12 col-sm-4 col-md-offset-2 col-md-2" >
                 <img src={"https://media.mercola.com/assets/images/food-facts/apple.jpg"} alt="apples"/>
             </div>
 
-            <div className="col-xs-12 col-sm-4 col-sm-offset-2 col-md-4">
+            <div className="col-xs-12 col-sm-4 col-sm-offset-2 col-md-4" id="first-section">
                 <h2 className="">
                   Connecting Vashon Families in need with Community Partners, Supporters, and 
                   Vashon Farms + Farmer Markets
@@ -49,25 +49,70 @@ class App extends Component {
 
         </div>
 
-        <div className="mid-section" id="second-section">
-          <div className="container">
-            <h1 className="">The Partnership also distributes vouchers for redemption at farmstands and markets on Vashon.</h1>
-            <p className="">Currently, the program partners include...</p>
-          </div>
+       <div className="withPadding row">
+            <div className="col-xs-12 col-sm-4 col-sm-offset-2 col-md-4" id="second-section">
+                <h2 className="">
+                  The Partnership also distributes vouchers for redemption at farmstands and markets on Vashon.
+                  Food Access Partnership partners with local non-profit organizations to ensure low resource communities
+                  have access to fresh food.
+                </h2>
+                <p className="">
+                  Currently the program partners include:
+                </p>
+                <p className="">
+                  <a href="http://www.vashonhousehold.org/">Vashon Household</a>
+                  <a href="http://vashonseniorcenter.org/">Vashon Senior Center</a>
+                  <a href="">Community Meals</a>
+                  <a href="http://www.vashonfoodbank.org/01_Home/index.shtml">Vashon Maury Community Food Bank</a>
+                  <a href="https://vashondoveproject.org/">The DOVE Project</a>
+                  <a href="https://www.vashoncommunitycare.org/">Vashon Community Care</a>
+                  <a href="https://ifchvashon.org/">Interfaith Council to Prevent Homelessness</a>
+                  {/* not sure they have a site? */}
+                  <a href="https://www.coopartnerships.org/new-blog/2018/5/31/comunidad-latina-de-vashon-organizamos">Communities of Opportunity, La Communidad</a>
+                  <a href="https://vyfs.org/">Vashon Youth and Family Services</a>
+
+                </p>
+            </div>
+            <div className="center-xs col-xs-12 col-sm-4 col-md-offset-2 col-md-2">
+                <img src={"https://upload.wikimedia.org/wikipedia/commons/thumb/f/f4/Honeycrisp.jpg/440px-Honeycrisp.jpg"} alt="pears"/>
+            </div>
         </div>
 
-        <div className="mid-section" id="third-section">
-          <div className="container">
-            <h1 className="">Meet the team!</h1>
-            <p className="">Fully run by volunteers, the food access partnership is made of chefs, farmers, and activists.</p>
-          </div>
+        <div className="withPadding row">
+            <div className="center-xs col-xs-12 col-sm-4 col-md-offset-2 col-md-2" >
+                <img src={""} alt="/"/>
+            </div>
+            <div className="col-xs-12 col-sm-4 col-sm-offset-2 col-md-4" id="third-section">
+                <h1 className="">Meet the team!</h1>
+                <p className="">
+                  Fully run by volunteers, the food access partnership is made of chefs, farmers, philanthropists, and activists.
+                </p>
+            </div>
         </div>
 
         <div className="container border"></div>
 
         <div className="container" id="footer">
-          <p className="">Like, comment and subscribe!</p>
-        </div>
+          <p className="">The Food Access Partnership's work has been made possible via
+          grants and donations from The Hendrix Foundation and Granny's Attic, as well 
+          from islanders like you. We have received additional donations from the
+          Wildwoods Foundation and Les Dames d'Escoffier of Seattle, effective 2016.
+          </p>
+          <p>
+          We encourage you to make a tax deductible contribution with cash or check at
+          the Market, through the mail (VIGA, P.O. Box 2894, Vashon, 98070) or online by 
+          selecting the 'Donate' button below.
+          </p>
+          <button type="button" id="button1">Donate</button>
+       </div>
+
+       <div className="container border"></div>
+       
+       <div className="container" id="footer">
+          <p>
+             Vashon Island Grower's Association &copy; 2019
+          </p>
+       </div>
 
       </div>
 
