cfdec28d1a58b328e86d86117cdcd1d8ca50c555
antonzheng <anton.zheng@gmail.com>
2019-02-05 22:48:08 -0800

    Updates

---
+++ precache-manifest.6cf2b1b53dbfc889030eca642ea7314a.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "2923c5cdb40a4b8c7654ebdcdddda66d",
+    "url": "/vashonward/static/media/volunteers.2923c5cd.png"
+  },
+  {
+    "revision": "0668b4d333996fe537101460e664bac3",
+    "url": "/vashonward/static/media/pears.0668b4d3.png"
+  },
+  {
+    "revision": "fe3f2881dfaff21a716f0115848b69d4",
+    "url": "/vashonward/static/media/hero_img.fe3f2881.png"
+  },
+  {
+    "revision": "7f322bf866fbf96b45dfbca53c0907d4",
+    "url": "/vashonward/static/media/blueberries.7f322bf8.png"
+  },
+  {
+    "revision": "aadf3c3bbd7c456792e7",
+    "url": "/vashonward/static/js/runtime~main.aadf3c3b.js"
+  },
+  {
+    "revision": "f1899496ed6e2751ad08",
+    "url": "/vashonward/static/js/main.f1899496.chunk.js"
+  },
+  {
+    "revision": "4a34e45d34cafb7623bd",
+    "url": "/vashonward/static/js/1.4a34e45d.chunk.js"
+  },
+  {
+    "revision": "f1899496ed6e2751ad08",
+    "url": "/vashonward/static/css/main.d19be0b8.chunk.css"
+  },
+  {
+    "revision": "25f7cff5b454559a422057b3c9805351",
+    "url": "/vashonward/index.html"
+  }
+];
